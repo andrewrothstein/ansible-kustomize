59dcac28421af9f3a0012097df9ef30201a6cdd5
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-06-28 14:01:00 -0400

    addl checksums

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -23,14 +23,20 @@ dl_ver() {
     local lchecksums="$DIR/${APP}_${ver}_checksums.txt"
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $url
+        curl -sSLf -o $lchecksums $url
     fi
 
     printf "  # %s\n" $url
     printf "  %s:\n" $ver
 
     dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums darwin arm64
     dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm64
+    dl $ver $lchecksums linux ppc64le
+    dl $ver $lchecksums linux s390x
+    dl $ver $lchecksums windows amd64
+    dl $ver $lchecksums windows arm64
 }
 
 # available version: curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases | fgrep tarball/kustomize
